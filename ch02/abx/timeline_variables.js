add0c8efa3d80eb17a3db86e84b6e873e95a4073
ta <kishiyama.t@gmail.com>
2023-06-24 21:28:46 +0900

    updated

---
+++ ch02/abx/timeline_variables.js
@@ -0,0 +1,66 @@
+var timeline_variables = [
+ {
+   "a": "espo-1.wav",
+   "b": "esupo-3.wav",
+   "x": "espo-2.wav",
+   "type": "tgt",
+   "id": 1,
+   "cor": "a"
+ },
+ {
+   "a": "esupo-1.wav",
+   "b": "espo-3.wav",
+   "x": "espo-2.wav",
+   "type": "tgt",
+   "id": 2,
+   "cor": "b"
+ },
+ {
+   "a": "esupo-1.wav",
+   "b": "espo-3.wav",
+   "x": "esupo-2.wav",
+   "type": "tgt",
+   "id": 3,
+   "cor": "a"
+ },
+ {
+   "a": "espo-1.wav",
+   "b": "esupo-3.wav",
+   "x": "esupo-2.wav",
+   "type": "tgt",
+   "id": 4,
+   "cor": "b"
+ },
+ {
+   "a": "epso-1.wav",
+   "b": "epuso-3.wav",
+   "x": "epso-2.wav",
+   "type": "tgt",
+   "id": 5,
+   "cor": "a"
+ },
+ {
+   "a": "epuso-1.wav",
+   "b": "epso-3.wav",
+   "x": "epso-2.wav",
+   "type": "tgt",
+   "id": 6,
+   "cor": "b"
+ },
+ {
+   "a": "epuso-1.wav",
+   "b": "epso-3.wav",
+   "x": "epuso-2.wav",
+   "type": "tgt",
+   "id": 7,
+   "cor": "a"
+ },
+ {
+   "a": "epso-1.wav",
+   "b": "epuso-3.wav",
+   "x": "epuso-2.wav",
+   "type": "tgt",
+   "id": 8,
+   "cor": "b"
+ }
+]
---
+++ ch02/cat/timeline_variables.js
@@ -0,0 +1,50 @@
+var timeline_variables = [
+ {
+   "audio": "espo-1.wav",
+   "item_id": 1
+ },
+ {
+   "audio": "esupo-1.wav",
+   "item_id": 2
+ },
+ {
+   "audio": "epso-1.wav",
+   "item_id": 3
+ },
+ {
+   "audio": "epuso-1.wav",
+   "item_id": 4
+ },
+ {
+   "audio": "espo-2.wav",
+   "item_id": 5
+ },
+ {
+   "audio": "esupo-2.wav",
+   "item_id": 6
+ },
+ {
+   "audio": "epso-2.wav",
+   "item_id": 7
+ },
+ {
+   "audio": "epuso-2.wav",
+   "item_id": 8
+ },
+ {
+   "audio": "espo-3.wav",
+   "item_id": 9
+ },
+ {
+   "audio": "esupo-3.wav",
+   "item_id": 10
+ },
+ {
+   "audio": "epso-3.wav",
+   "item_id": 11
+ },
+ {
+   "audio": "epuso-3.wav",
+   "item_id": 12
+ }
+]
